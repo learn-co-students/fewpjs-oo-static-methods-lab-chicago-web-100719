722e5fbd52179136151bd8b60dc259ba323ae154
Janae Hall <janaeelisa@gmail.com>
2019-12-02 10:33:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9' -]+/g, '')
+  }
+
+  static titleize(string) {
+    let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let splitString = string.split(' ')
+    let newString = splitString.map(word => {
+      if (!words.includes(word) || splitString.indexOf(word) === 0) return this.capitalize(word)
+      else return word
+    })
+    return newString.join(' ')
+  }
 }
