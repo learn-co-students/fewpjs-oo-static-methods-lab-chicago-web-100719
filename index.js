04cba58a9bd365f4047916e9d9645e6b4507c236
Valeria <valeria.gutsuliak@gmail.com>
2019-12-02 11:30:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^-,'A-Za-z0-9' -]/g, '')
+
+  }
+  static titleize(string){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let restWord = []
+    let allWords = string.split(" ")
+    for (let n = 0; n < allWords.length; n++ ){
+      if(n == 0){
+        restWord.push(this.capitalize(allWords[n]))
+      } else {
+        if(exceptions.includes(allWords[n])){
+          restWord.push(allWords[n])
+        } else {
+          restWord.push(this.capitalize(allWords[n]))
+        }
+
+      }
+    }
+    return restWord.join( " ")
+
+  }
+  
 }
+
