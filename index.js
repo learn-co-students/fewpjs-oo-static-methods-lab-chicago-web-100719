3aadb92d0b96282c1e0e0d6e1c4f4a6448a3e5c8
NaveedG <nganjani@protonmail.com>
2019-12-03 07:57:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    let arr = []
+    let words = string.split(' ')
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    for(let x = 0; x < words.length; x++) {
+      if (x == 0) {
+        arr.push(this.capitalize(words[x]))
+      } else {
+      if (exceptions.includes(words[x])) {
+        arr.push(words[x])
+      } else {
+        arr.push(this.capitalize(words[x]))
+        }
+      }
+    }
+    return arr.join(' ')
+  }
+
 }
